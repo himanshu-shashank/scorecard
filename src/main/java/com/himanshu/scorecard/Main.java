2a4d2caaab53f5022ad21f6a8faffb2626083659
Himanshu Shashank <himanshu.shashank@oracle.com>
2021-12-06 14:11:03 +0530

    hardcoding input

--- src/main/java/com/himanshu/scorecard/Main.java
+++ src/main/java/com/himanshu/scorecard/Main.java
@@ -15,8 +15,10 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         BattingSide team1 = new BattingSide("Team 1");
+        System.out.printf("Batting Order for %s\n", team1.getName());
         for(int i=0; i<playersEachTeam; i++) {
             Player player = new Player("P"+(i+1));
+            System.out.printf("%s\n",player.getName());
             team1.addPlayer(player);
             if(i==0)
                 team1.setOnStrikePlayer(player);
@@ -27,28 +29,43 @@ public class Main {
         }
 
         ScoreCalculatorService scoreCalculator1 = new ScoreCalculatorService(team1);
-        for(int i=0; i<overs; i++) {
-            System.out.printf("Over %d :\n",(i+1));
+        String[] ball = new String[]{"1","1","1","1","1","2"};
+            System.out.printf("Over %d :\n",(1));
             int j;
+            int k=0;
             for(j=1; j<=6;j++) {
-                String score = scanner.nextLine().trim();
-                if(scoreCalculator1.needToBallMore(score))
+                String score = ball[k++];
+                System.out.printf("%s\n",(score));
+                if (scoreCalculator1.needToBallMore(score))
                     j--;
 
                 scoreCalculator1.CalculateScore(score, j);
-                if(!team1.canBatMore)
+                if (!team1.canBatMore)
                     break;
-
             }
             scorecardPrinter.printScoreCard(team1);
-            if(!team1.canBatMore)
+
+        ball = new String[]{"w","4","4","wd","w","1","6"};
+        System.out.printf("Over %d :\n",(2));
+        k=0;
+        for(j=1; j<=6 ;j++) {
+            String score = ball[k++];
+            System.out.printf("%s\n",(score));
+            if (scoreCalculator1.needToBallMore(score))
+                j--;
+
+            scoreCalculator1.CalculateScore(score, j);
+            if (!team1.canBatMore)
                 break;
         }
+        scorecardPrinter.printScoreCard(team1);
 
         BattingSide team2 = new BattingSide("Team 2");
+        System.out.printf("Batting Order for %s\n", team2.getName());
         ScoreCalculatorService scoreCalculator2 = new ScoreCalculatorService(team2);
         for(int i=0;i<playersEachTeam;i++) {
             Player player = new Player("P"+(playersEachTeam+i+1));
+            System.out.printf("%s\n",player.getName());
             team2.addPlayer(player);
             if(i==0)
                 team2.setOnStrikePlayer(player);
@@ -58,11 +75,12 @@ public class Main {
                 team2.addPlayertoQueue(player);
         }
 
-        for(int i=0; i<overs; i++) {
-            int j;
-            System.out.printf("Over %d :\n",(i+1));
+        ball = new String[]{"4","6","w","w","1","1"};
+        k=0;
+            System.out.printf("Over %d :\n",(1));
             for(j=1;j<=6;j++) {
-                String score = scanner.nextLine().trim();
+                String score = ball[k++];
+                System.out.printf("%s\n",(score));
                 if(scoreCalculator2.needToBallMore(score))
                     j--;
 
@@ -71,9 +89,20 @@ public class Main {
                     break;
             }
             scorecardPrinter.printScoreCard(team2);
-            if(!team2.canBatMore)
+        ball = new String[]{"6","1","w","w"};
+        k=0;
+        System.out.printf("Over %d :\n",(1));
+        for(j=1;j<=6;j++) {
+            String score = ball[k++];
+            System.out.printf("%s\n",(score));
+            if(scoreCalculator2.needToBallMore(score))
+                j--;
+
+            scoreCalculator2.CalculateScore(score, j);
+            if(!team2.canBatMore || team2.hasScoredMore(team1, j))
                 break;
         }
+        scorecardPrinter.printScoreCard(team2);
 
         scoreCalculator2.calculateFinalResult(team1, team2);
 
